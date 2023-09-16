b0b0e071f4cf038a9ae2f65dda84939cf6bccda1
King <kurvanovjurat@gmail.com>
2023-09-16 11:59:19 +0600

    добавил открытие и чтение текстовых файлов

--- app/src/main/java/com/example/exapplication/Window.kt
+++ app/src/main/java/com/example/exapplication/Window.kt
@@ -1,17 +1,63 @@
 package com.example.exapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
 
 class Window : AppCompatActivity() {
+    private var FILE_NAME = "content.txt"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_window)
+    }
 
-        val intent = intent
+    fun saveText(view: View) {
+        var fos: FileOutputStream? = null
 
-        if (intent != null && intent.hasExtra("key_from_fragment")){
+        try {
+            val editBox = findViewById<EditText>(R.id.editor)
+            val text = editBox.text.toString()
 
+            fos = openFileOutput(FILE_NAME, Context.MODE_PRIVATE)
+            fos.write(text.toByteArray())
+            Toast.makeText(this, "Файл сохранен", Toast.LENGTH_SHORT).show()
+        } catch (ex: IOException) {
+            Toast.makeText(this, ex.message, Toast.LENGTH_SHORT).show()
+        } finally {
+            try {
+                fos?.close()
+            } catch (ex: IOException) {
+                Toast.makeText(this, ex.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun openText(view: View) {
+        var fin: FileInputStream? = null
+        val textView = findViewById<TextView>(R.id.text)
+
+        try {
+            fin = openFileInput(FILE_NAME)
+            val bytes = ByteArray(fin.available())
+            fin.read(bytes)
+            val text = String(bytes)
+            textView.text = text
+        } catch (ex: IOException) {
+            Toast.makeText(this, ex.message, Toast.LENGTH_SHORT).show()
+        } finally {
+            try {
+                fin?.close()
+            } catch (ex: IOException) {
+                Toast.makeText(this, ex.message, Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
